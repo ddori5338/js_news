f382aadf88520c8688cd72e147be0686612dbf54
youngchangyoo <ddori5338@hanmail.net>
2024-02-23 22:53:36 +0900

    chapter 13

--- main.js
+++ main.js
@@ -6,10 +6,21 @@ menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategor
 const getNews = async (category, keyword) => {
     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us${category ? "&category=" + category : ""}${keyword ? "&q=" + keyword : ""}&apiKey=${API_KEY}`);
     const url = new URL(`https://ddori5338-news.netlify.app/top-headlines?${category ? "&category=" + category : ""}${keyword ? "&q=" + keyword : ""}`);
-    const response = await fetch(url);
-    const data = await response.json();
-    newsList = data.articles;
-    render();
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        if (response.status === 200) {
+            if (data.articles.length === 0) {
+                throw new Error("No result for this search");
+            }
+            newsList = data.articles;
+            render();
+        } else {
+            throw new Error(data.message);
+        }
+    } catch (error) {
+        errorRender(error.message);
+    }
 }
 
 const getLatestNews = async () => {
@@ -83,6 +94,13 @@ const render = () => {
     }).join('');
 
     document.getElementById('news-board').innerHTML = newsHTML;
+}
+
+const errorRender = (errorMessage) => {
+    const errorHTML = `<div class="alert alert-danger" role="alert">
+        ${errorMessage}
+    </div>`;
+    document.getElementById('news-board').innerHTML = errorHTML;
 }
 
 getLatestNews();
